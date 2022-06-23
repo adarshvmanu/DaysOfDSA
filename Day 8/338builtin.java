384c3641f5d1a02454c7099b7d19b504ae56f166
Adarsh Varghese Manu <adarshvarghesemanu18@gmail.com>
2022-06-24 00:54:25 +0530

    Day 8

--- Day 8/338builtin.java
+++ Day 8/338builtin.java
@@ -4,7 +4,7 @@ class Solution {
         int[] ans = new int[n+1];
         for(int i=0; i<=n; i++)
         {
-            ans[i] = Integer.bitCount(i);
+          ans[i] = Integer.bitCount(i);
         }
         return ans;
     }
